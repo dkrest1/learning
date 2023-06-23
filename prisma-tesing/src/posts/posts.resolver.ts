789aa6614e4c99133453f1c1fae6bf10cf8665ac
Akande Oluwatosin <aktosin1@gmail.com>
2023-06-23 05:53:58 +0100

    learning: testing

--- prisma-tesing/src/posts/posts.resolver.ts
+++ prisma-tesing/src/posts/posts.resolver.ts
@@ -3,32 +3,61 @@ import { PostsService } from './posts.service';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Post as PostModel } from '@prisma/client';
+import { title } from 'process';
 
 @Resolver(() => Post)
 export class PostsResolver {
   constructor(private readonly postsService: PostsService) {}
 
   @Mutation(() => Post)
-  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
-    return;
+  async createPostDraft(
+    @Args('createPostInput')
+    createPostInput: {
+      title: string;
+      content: string;
+      authorEmail: string;
+    },
+  ): Promise<PostModel> {
+    const { title, content, authorEmail } = createPostInput;
+    return await this.postsService.createPost({
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    });
   }
 
   @Query(() => [Post], { name: 'posts' })
-  findAll() {
-    return;
+  async findAll(): Promise<PostModel[]> {
+    return await this.postsService.posts({});
+  }
+
+  @Query(() => [Post], { name: 'publised Post' })
+  async getPublishedPost(): Promise<PostModel[]> {
+    return await this.postsService.posts({ where: { published: true } });
   }
 
   @Query(() => Post, { name: 'post' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return;
-  }
-  @Mutation(() => Post)
-  updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
-    return;
+  async findOne(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<PostModel> {
+    return await this.postsService.post({ id });
   }
 
   @Mutation(() => Post)
-  removePost(@Args('id', { type: () => Int }) id: number) {
-    return;
+  async updatePost(
+    @Args('id') id: number,
+    @Args('updatePostInput')
+    updatePostInput: { title: string; content: string },
+  ): Promise<PostModel> {
+    return await this.postsService.updatePost({
+      where: { id },
+      data: updatePostInput,
+    });
+  }
+
+  @Mutation(() => Post)
+  async removePost(@Args('id', { type: () => Int }) id: number) {
+    return await this.postsService.deletePost({ id });
   }
 }
--- prisma-tesing/src/users/users.resolver.ts
+++ prisma-tesing/src/users/users.resolver.ts
@@ -3,36 +3,46 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
-import { User as UserModel, Post as PostModel } from '@prisma/client';
+import { User as UserModel } from '@prisma/client';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Mutation(() => User)
-  registerUser(
+  async registerUser(
     @Args('registerUser') registerUserInput: { name?: string; email: string },
   ): Promise<UserModel> {
-    return this.usersService.createUser(registerUserInput);
+    return await this.usersService.createUser(registerUserInput);
   }
 
   @Query(() => [User], { name: 'users' })
   findAll() {
-    return;
+    return this.usersService.users({});
   }
 
   @Query(() => User, { name: 'user' })
   findOne(@Args('id', { type: () => Int }) id: number) {
-    return;
+    return this.usersService.user({ id });
   }
 
   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return;
+  async updateUser(
+    @Args('id') id: number,
+    @Args('updateUserInput')
+    updateUserInput: {
+      name?: string;
+      email: string;
+    },
+  ) {
+    return await this.usersService.updateUser({
+      where: { id },
+      data: updateUserInput,
+    });
   }
 
   @Mutation(() => User)
   removeUser(@Args('id', { type: () => Int }) id: number) {
-    return;
+    return this.usersService.deleteUser({ id });
   }
 }
